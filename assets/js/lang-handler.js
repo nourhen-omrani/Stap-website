09c6954854f3092129fc5926da4f78b8ce486bd7
nourhen-omrani <nourhen.omrani@outlook.fr>
2025-04-14 23:00:03 +0100

    Fix translation

--- assets/js/lang-handler.js
+++ assets/js/lang-handler.js
@@ -6,3 +6,31 @@ document.getElementById("langSelector").addEventListener("change", function () {
     });
   });
 
+  function changeLang(lang) {
+    const langLabel = document.getElementById("currentLang");
+    const langIcon = document.querySelector(".dropdown-toggle img");
+
+    if (lang === "fr") {
+      langLabel.innerText = "Français";
+      langIcon.src = "https://flagcdn.com/w40/fr.png";
+    } else if (lang === "en") {
+      langLabel.innerText = "English";
+      langIcon.src = "https://flagcdn.com/w40/gb.png";
+    }
+
+    // Appelle ici ta logique i18n Angular ou JS (par ex: translateService.use(lang))
+    // translateService.use(lang); // pour Angular avec ngx-translate
+  }
+
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    const langBtn = document.querySelector(".language-selector .btn");
+
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-scrolled");
+      langBtn.classList.add("lang-scrolled");
+    } else {
+      navbar.classList.remove("navbar-scrolled");
+      langBtn.classList.remove("lang-scrolled");
+    }
+  });
